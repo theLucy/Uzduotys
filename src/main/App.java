6ea0baaa80f0c38dde68a30d4cdb10a31fd5ee83
Lucy <info@thelucy.lt>
2023-03-10 13:50:59 +0200

    Attempt to make it faster by replacing strings with char arrays(Not working)

--- src/main/App.java
+++ src/main/App.java
@@ -1,28 +1,40 @@
 package main;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
         System.out.println(longestPalindrome("babad")); // bab
         System.out.println(longestPalindrome("cbbd")); // bb
         long start = System.currentTimeMillis();
-        System.out.println(longestPalindrome("jkexvzsqshsxyytjmmhauoyrbxlgvdovlhzivkeixnoboqlfemfzytbolixqzwkfvnpacemgpotjtqokrqtnwjpjdiidduxdprngvitnzgyjgreyjmijmfbwsowbxtqkfeasjnujnrzlxmlcmmbdbgryknraasfgusapjcootlklirtilujjbatpazeihmhaprdxoucjkynqxbggruleopvdrukicpuleumbrgofpsmwopvhdbkkfncnvqamttwyvezqzswmwyhsontvioaakowannmgwjwpehcbtlzmntbmbkkxsrtzvfeggkzisxqkzmwjtbfjjxndmsjpdgimpznzojwfivgjdymtffmwtvzzkmeclquqnzngazmcfvbqfyudpyxlbvbcgyyweaakchxggflbgjplcftssmkssfinffnifsskmsstfclpjgblfggxhckaaewyygcbvblxypduyfqbvfcmzagnznquqlcemkzzvtwmfftmydjgvifwjoznzpmigdpjsmdnxjjfbtjwmzkqxsizkggefvztrsxkkbmbtnmzltbchepwjwgmnnawokaaoivtnoshywmwszqzevywttmaqvncnfkkbdhvpowmspfogrbmuelupcikurdvpoelurggbxqnykjcuoxdrpahmhiezaptabjjulitrilkltoocjpasugfsaarnkyrgbdbmmclmxlzrnjunjsaefkqtxbwoswbfmjimjyergjygzntivgnrpdxuddiidjpjwntqrkoqtjtopgmecapnvfkwzqxilobtyzfmeflqobonxiekvizhlvodvglxbryouahmmjtyyxshsqszvxekj"));
+        System.out.println(longestPalindrome("twbiqwtafgjbtolwprpdnymaatlbuacrmzzwhkpvuwdtyfjsbsqxrlxxtqkjlpkvpxmlajdmnyepsmczmmfdtjfbyybotpoebilayqzvqztqgddpcgpelwmriwmoeeilpetbxoyktizwcqeeivzgxacuotnlzutdowiudwuqnghjgoeyojikjhlmcsrctvnahnoapmkcrqmwixpbtirkasbyajenknuccojooxfwdeflmxoueasvuovcayisflogtpxtbvcxfmydjupwihnxlpuxxcclbhvutvvshcaikuedhyuksbwwjsnssizoedjkbybwndxpkwcdxaexagazztuiuxphxcedqstahmevkwlayktrubjypzpaiwexkwbxcrqhkpqevhxbyipkmlqmmmogrnexsztsbkvebjgybrolttvnidnntpgvsawgaobycfaaviljsvyuaanguhohsepbthgjyqkicyaxkytshqmtxhilcjxdpcbmvnpippdrpggyohwyswuydyrhczlxyyzregpvxyfwpzvmjuukswcgpenygmnfwdlryobeginxwqjhxtmbpnccwdaylhvtkgjpeyydkxcqarkwvrmwbxeetmhyoudfuuwxcviabkqyhrvxbjmqcqgjjepmalyppymatylhdrazxytixtwwqqqlrcusxyxzymrnryyernrxbgrphsioxrxhmxwzsytmhnosnrpwtphaunprdtbpwapgjjqcnykgspjsxslxztfsuflijbeebwyyowjzpsbjcdabxmxhtweppffglvhfloprfavduzbgkw"));
         long finish = System.currentTimeMillis();
         System.out.println(finish-start + "ms");
     }
 
     public static String longestPalindrome(String s) {
-        StringBuilder sb;
-        String normal, reversed;
         String result = s.substring(0,1);
 
+        char[] a = s.toCharArray();
+        char[] b = new char[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[a.length-1-i];
+        }
+
         for (int i = 0; i < s.length(); i++) {
+            if((s.length()-1)-i <= result.length()) break;
             for (int j = s.length() - 1; j > i; j--) {
-                sb = new StringBuilder(s.substring(i, j + 1));
-                normal = sb.toString();
-                sb.reverse();
-                reversed = sb.toString();
-                if (normal.length() > result.length() && normal.equals(reversed)) {
-                    result = normal;
+
+                if(j-i <= result.length()) break;
+
+                boolean eq = Arrays.equals(
+                        Arrays.copyOfRange(a, i, j+1),
+                        Arrays.copyOfRange(b, b.length-j-1, j-2)
+                );
+
+                if (eq) {
+                    result = new String(Arrays.copyOfRange(a, i, j+1));
                 }
             }
         }
