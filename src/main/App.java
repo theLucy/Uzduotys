9018922897d7199c895b813976d29e15cc2ed35e
Lucy <info@thelucy.lt>
2023-03-10 15:39:10 +0200

    Solve taslk

--- src/main/App.java
+++ src/main/App.java
@@ -1,29 +1,57 @@
 package main;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-
-        var result1 = List.of(
-                twoSum(new int[]{2, 7, 11, 15}, 9),
-                twoSum(new int[]{3, 2, 4}, 6),
-                twoSum(new int[]{3, 3}, 6),
-                twoSum(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21}, 26)
-        );
-
-        result1.forEach(x -> x.map(Arrays::toString).ifPresent(System.out::println));
-
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
     }
 
-    public static Optional<int[]> twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i + 1; j < nums.length; j++)
-                if (nums[i] + nums[j] == target)
-                    return Optional.of(new int[]{i, j});
+    public static int romanToInt(String s) {
 
-        return Optional.empty();
+        int result = 0;
+
+        char[] romanSymbols = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+        int ptr = 0;
+
+        Map<Character, Integer> rToIntMap = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000
+        );
+
+
+        for (int i = 0; i < romanSymbols.length && ptr < s.length();) {
+            char r = romanSymbols[i];
+            if (s.charAt(ptr) == r) {
+                result += rToIntMap.get(r);
+            } else {
+                ptr--;
+            }
+
+            if((ptr+2) < s.length() && s.charAt(ptr+1) != r &&
+                    s.charAt(ptr+2) == r) {
+                result += rToIntMap.get(r) - rToIntMap.get(s.charAt(ptr+1));
+                ptr += 3;
+                i++;
+            } else if((ptr+1) < s.length() && s.charAt(ptr+1) == r) {
+                result += rToIntMap.get(r);
+                ptr+=2;
+            } else {
+                i++;
+                ptr++;
+            }
+        }
+
+
+
+
+        return result;
     }
 }
