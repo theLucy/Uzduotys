fb1681390c9d688ebd43354ce5c82ac3f57eef2c
Lucy <info@thelucy.lt>
2023-03-15 15:57:23 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -1,29 +1,50 @@
 package main;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 public class App {
     public static void main(String[] args) {
-
-        var result1 = List.of(
-                twoSum(new int[]{2, 7, 11, 15}, 9),
-                twoSum(new int[]{3, 2, 4}, 6),
-                twoSum(new int[]{3, 3}, 6),
-                twoSum(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21}, 26)
-        );
-
-        result1.forEach(x -> x.map(Arrays::toString).ifPresent(System.out::println));
-
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"})); // "fl"
+        System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"})); // ""
     }
 
-    public static Optional<int[]> twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i + 1; j < nums.length; j++)
-                if (nums[i] + nums[j] == target)
-                    return Optional.of(new int[]{i, j});
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder prefix = new StringBuilder();
+        boolean isCommonChar = true;
 
-        return Optional.empty();
+
+        int shortestWord = Integer.MAX_VALUE;
+        for (int i = 0; i < strs.length; i++) {
+            shortestWord = Math.min(shortestWord, strs[i].length());
+        }
+
+        char[][] wordChars = new char[strs.length][];
+
+        for (int i = 0; i < strs.length; i++) {
+            wordChars[i] = strs[i].toCharArray();
+        }
+
+        for (int i = 0; i < shortestWord; i++) {
+            char x = wordChars[0][i];
+            for (int j = 1; j < strs.length; j++) {
+                if (x != wordChars[j][i]) return prefix.toString();
+            }
+            prefix.append(x);
+        }
+
+        return prefix.toString();
     }
+
+    /*public static String longestCommonPrefix2(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String result = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(result)) {
+                result = result.substring(0, result.length() - 1);
+            }
+        }
+        return result;
+    }*/
 }
