4fcf49bf2dc66c6e9822a6860d9d34e73ea5078e
Lucy <info@thelucy.lt>
2023-03-17 11:26:29 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -1,29 +1,35 @@
 package main;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 public class App {
     public static void main(String[] args) {
+        int[] nums = {3, 9, 2, 3, 1, 7, 2, 3, 5}; // Input array
+        int val = 3; // Value to remove
+        int[] expectedNums = {1, 2, 2, 5, 7, 9}; // The expected answer with correct length.
+        // It is sorted with no values equaling val.
 
-        var result1 = List.of(
-                twoSum(new int[]{2, 7, 11, 15}, 9),
-                twoSum(new int[]{3, 2, 4}, 6),
-                twoSum(new int[]{3, 3}, 6),
-                twoSum(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21}, 26)
-        );
+        int k = removeElement(nums, val); // Calls your implementation
 
-        result1.forEach(x -> x.map(Arrays::toString).ifPresent(System.out::println));
+        //assert k == expectedNums.length;
+        System.out.printf("%d | %d || %b\n\n", k, expectedNums.length, k == expectedNums.length);
 
+        Arrays.sort(nums, 0, k); // Sort the first k elements of nums
+        for (int i = 0; i < expectedNums.length; i++) {
+            System.out.printf("%d | %d || %b\n", nums[i], expectedNums[i], nums[i] == expectedNums[i]);
+            //assert nums[i] == expectedNums[i];
+        }
     }
 
-    public static Optional<int[]> twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i + 1; j < nums.length; j++)
-                if (nums[i] + nums[j] == target)
-                    return Optional.of(new int[]{i, j});
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
 
-        return Optional.empty();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        return index;
     }
 }
