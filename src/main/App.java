0ee2a0e9afefccfab7cd10f976c19c61ebff23dc
Lucy <info@thelucy.lt>
2023-03-21 10:21:25 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -1,29 +1,54 @@
 package main;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 public class App {
     public static void main(String[] args) {
-
-        var result1 = List.of(
-                twoSum(new int[]{2, 7, 11, 15}, 9),
-                twoSum(new int[]{3, 2, 4}, 6),
-                twoSum(new int[]{3, 3}, 6),
-                twoSum(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21}, 26)
-        );
-
-        result1.forEach(x -> x.map(Arrays::toString).ifPresent(System.out::println));
-
+        var test1 = new int[]{1, 2, 3};
+        var test2 = new int[]{4, 3, 2, 1};
+        var test3 = new int[]{9};
+        System.out.println(Arrays.toString(plusOne2(test1))); // Expected [1,2,4]
+        System.out.println(Arrays.toString(plusOne2(test2))); // Expected [4,3,2,2]
+        System.out.println(Arrays.toString(plusOne2(test3))); // Expected [1,0]
     }
 
-    public static Optional<int[]> twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i + 1; j < nums.length; j++)
-                if (nums[i] + nums[j] == target)
-                    return Optional.of(new int[]{i, j});
+    public static int[] plusOne(int[] digits) {
+        boolean all9 = true;
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != 9) {
+                all9 = false;
+                break;
+            }
+        }
+        if (all9) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            return result;
+        }
 
-        return Optional.empty();
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        return digits;
+    }
+
+    public static int[] plusOne2(int[] digits) {
+        int carry = 1;
+        int[] result = new int[digits.length];
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int val = digits[i] + carry;
+            result[i] = val % 10;
+            carry = val / 10;
+        }
+        if (carry == 1) {
+            result = new int[digits.length + 1];
+            result[0] = 1;
+        }
+        return result;
     }
 }
