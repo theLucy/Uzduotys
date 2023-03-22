544117406e4e2ae6ad9fddb0e11f5c547cdec11d
Lucy <info@thelucy.lt>
2023-03-22 13:29:30 +0200

    Convert ListNode to generic type. Shorten test list creation.

--- src/main/App.java
+++ src/main/App.java
@@ -6,22 +6,19 @@ import java.util.Set;
 public class App {
 
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(3);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(0);
-        ListNode node4 = new ListNode(-4);
+        ListNode<Integer> node4 = new ListNode<>(-4);
+        ListNode<Integer> node3 = new ListNode<>(0, node4);
+        ListNode<Integer> node2 = new ListNode<>(2, node3);
+        ListNode<Integer> node1 = new ListNode<>(3, node2);
 
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node2;
+        node4.next = node1;
 
         System.out.println(hasCycle(node1));
 
     }
 
-    public static boolean hasCycle(ListNode head) {
-        Set<ListNode> nodes = new HashSet<>();
+    public static <T> boolean hasCycle(ListNode<T> head) {
+        Set<ListNode<T>> nodes = new HashSet<>();
         while (head != null) {
             if (!nodes.add(head))
                 return true;
@@ -30,13 +27,18 @@ public class App {
         return false;
     }
 
-    static class ListNode {
-        int val;
-        ListNode next;
+    static class ListNode<T> {
+        T val;
+        ListNode<T> next;
 
-        ListNode(int x) {
+        ListNode(T x) {
             val = x;
             next = null;
+        }
+
+        ListNode(T x, ListNode<T> n) {
+            val = x;
+            next = n;
         }
     }
 }
