0b4ab3e1567c1d4d7f0c40ed3f27a19dd1ee6409
Lucy <info@thelucy.lt>
2023-03-22 16:01:42 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -1,29 +1,19 @@
 package main;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 public class App {
     public static void main(String[] args) {
-
-        var result1 = List.of(
-                twoSum(new int[]{2, 7, 11, 15}, 9),
-                twoSum(new int[]{3, 2, 4}, 6),
-                twoSum(new int[]{3, 3}, 6),
-                twoSum(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21}, 26)
-        );
-
-        result1.forEach(x -> x.map(Arrays::toString).ifPresent(System.out::println));
-
+        System.out.println(titleToNumber("A")); // Expected 1
+        System.out.println(titleToNumber("AB")); // Expected 28
+        System.out.println(titleToNumber("ZY")); // Expected 701
     }
 
-    public static Optional<int[]> twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i + 1; j < nums.length; j++)
-                if (nums[i] + nums[j] == target)
-                    return Optional.of(new int[]{i, j});
-
-        return Optional.empty();
+    public static int titleToNumber(String columnTitle) {
+        int n = columnTitle.length() - 1;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            int digit = columnTitle.charAt(n - i) - 'A' + 1;
+            sum += digit * Math.pow(26, i);
+        }
+        return sum;
     }
 }
