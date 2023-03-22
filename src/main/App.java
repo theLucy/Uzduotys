c8a5e5690615600d4b12750d8d3fa93873a69423
Lucy <info@thelucy.lt>
2023-03-22 11:44:13 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -1,29 +1,42 @@
 package main;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashSet;
+import java.util.Set;
 
 public class App {
+
     public static void main(String[] args) {
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(-4);
 
-        var result1 = List.of(
-                twoSum(new int[]{2, 7, 11, 15}, 9),
-                twoSum(new int[]{3, 2, 4}, 6),
-                twoSum(new int[]{3, 3}, 6),
-                twoSum(new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21}, 26)
-        );
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
 
-        result1.forEach(x -> x.map(Arrays::toString).ifPresent(System.out::println));
+        System.out.println(hasCycle(node1));
 
     }
 
-    public static Optional<int[]> twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i + 1; j < nums.length; j++)
-                if (nums[i] + nums[j] == target)
-                    return Optional.of(new int[]{i, j});
+    public static boolean hasCycle(ListNode head) {
+        Set<ListNode> nodes = new HashSet<>();
+        while (head != null) {
+            if (!nodes.add(head))
+                return true;
+            head = head.next;
+        }
+        return false;
+    }
 
-        return Optional.empty();
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
     }
 }
