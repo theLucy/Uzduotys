50f4694b7d90ec4a6bcabf069c9b61b208a077e0
Lucy <info@thelucy.lt>
2023-03-23 16:07:19 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -1,8 +1,43 @@
 package main;
 
 public class App {
+
+    public static ListNode makeList(int... values) {
+        ListNode tail = new ListNode(values[values.length - 1]);
+        ListNode node = null;
+        for (int i = 0; i < values.length - 1; i++) {
+            node = new ListNode(values[values.length - 1 - 1 - i], tail);
+            tail = node;
+        }
+        return node;
+    }
+
+    public static void printList(ListNode head) {
+        System.out.print("List: { ");
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println("}");
+    }
+
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+
+        ListNode node1 = makeList(1, 2, 3, 4, 5);
+        ListNode node2 = makeList(1, 2);
+        ListNode node3 = null;
+
+        printList(node1);
+        printList(node2);
+        printList(node3);
+
+        node1 = sol.reverseList(node1);
+        node2 = sol.reverseList(node2);
+        node3 = sol.reverseList(node3);
+
+        printList(node1);
+        printList(node2);
+        printList(node3);
     }
 }
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,23 @@
 package main;
 
 public class Solution {
-    
+    public ListNode reverseList(ListNode head) {
+        ListNode next1;
+        ListNode next2;
+        ListNode current = head;
+        ListNode previous = null;
+
+        while (current != null) {
+            next1 = current.next;
+            current.next = previous;
+            if (next1 == null) return current;
+            next2 = next1.next;
+            next1.next = current;
+            if (next2 == null) return next1;
+            previous = next1;
+            current = next2;
+
+        }
+        return current;
+    }
 }
