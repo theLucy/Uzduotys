568f0074c671d8eec6ab2aea32dfd3b94f2a04cd
Lucy <info@thelucy.lt>
2023-03-23 11:14:33 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -1,8 +1,37 @@
 package main;
 
 public class App {
+
+    public static ListNode makeList(int... values) {
+        ListNode tail = new ListNode(values[values.length - 1]);
+        ListNode node = null;
+        for (int i = 0; i < values.length - 1; i++) {
+            node = new ListNode(values[values.length - 1 - 1 - i], tail);
+            tail = node;
+        }
+        return node;
+    }
+
+    public static void printList(ListNode head) {
+        System.out.print("List: { ");
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println("}");
+    }
+
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+        ListNode head1 = makeList(7, 7, 7, 7);
+
+        ListNode head = head1;
+
+        printList(head);
+
+        head = sol.removeElements(head1, 7);
+
+        printList(head);
+
     }
 }
