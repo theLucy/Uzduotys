29df85f130044520edcc143cefffc6ca05331c0f
Lucy <info@thelucy.lt>
2023-03-23 14:37:16 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,8 @@ package main;
 public class App {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+        System.out.println(sol.isIsomorphic("egg", "add")); // Expected true
+        System.out.println(sol.isIsomorphic("foo", "bar")); // Expected false
+        System.out.println(sol.isIsomorphic("paper", "title")); // Expected true
     }
 }
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,17 @@
 package main;
 
 public class Solution {
-    
+    public boolean isIsomorphic(String s, String t) {
+        int[] map1 = new int[128];
+        int[] map2 = new int[128];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map1[s.charAt(i)] != map2[t.charAt(i)])
+                return false;
+
+            map1[s.charAt(i)] = i + 1;
+            map2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
 }
