8d86652143077d59e362d68043083876703139cc
Lucy <info@thelucy.lt>
2023-03-24 09:34:06 +0200

    Better solution

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,15 +1,13 @@
 package main;
 
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int prev = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == prev) return true;
-            prev = nums[i];
-        }
+        HashSet<Integer> unique = new HashSet<>();
+        for (int num : nums)
+            if (!unique.add(num)) return true;
+
         return false;
     }
 }
