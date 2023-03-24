abbcb5a3c0dcfe51169388cec85c5562bf2fcf32
Lucy <info@thelucy.lt>
2023-03-24 13:21:05 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,8 @@ package main;
 public class App {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+        System.out.println(sol.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3)); // Expected true
+        System.out.println(sol.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1)); // Expected true
+        System.out.println(sol.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2)); // Expected false
     }
 }
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,20 @@
 package main;
 
+import java.util.HashMap;
+
 public class Solution {
-    
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> mapas = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (mapas.containsKey(nums[i])) {
+                if (Math.abs(i - mapas.get(nums[i])) <= k) {
+                    return true;
+                }
+                mapas.replace(nums[i], i);
+            } else {
+                mapas.put(nums[i], i);
+            }
+        }
+        return false;
+    }
 }
