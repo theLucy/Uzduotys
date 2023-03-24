77e022e86bd5527309c6bc998aeb8060953f1d10
Lucy <info@thelucy.lt>
2023-03-24 09:01:12 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,8 @@ package main;
 public class App {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+        System.out.println(sol.containsDuplicate(new int[]{1, 2, 3, 1})); // Expected true
+        System.out.println(sol.containsDuplicate(new int[]{1, 2, 3, 4})); // Expected false
+        System.out.println(sol.containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})); // Expected true
     }
 }
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,15 @@
 package main;
 
+import java.util.Arrays;
+
 public class Solution {
-    
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int prev = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == prev) return true;
+            prev = nums[i];
+        }
+        return false;
+    }
 }
