2fcd06bf51a2bd8c2df9b5c3d402143b1ace761f
Lucy <info@thelucy.lt>
2023-03-24 15:59:00 +0200

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,14 @@ package main;
 public class App {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+        System.out.println(sol.summaryRanges(new int[]{0, 1, 2, 4, 5, 7}));
+        System.out.println(sol.summaryRanges(new int[]{0, 2, 3, 4, 6, 8, 9}));
+        System.out.println(sol.summaryRanges(new int[]{}));
+        System.out.println(sol.summaryRanges(new int[]{0}));
+        System.out.println(sol.summaryRanges(new int[]{0, 1}));
+        System.out.println(sol.summaryRanges(new int[]{0, 2}));
+        System.out.println(sol.summaryRanges(new int[]{0, 2, 4}));
+        System.out.println(sol.summaryRanges(new int[]{0, 2, 3}));
+        System.out.println(sol.summaryRanges(new int[]{0, 1, 3}));
     }
 }
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,35 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        StringBuilder sb;
+
+        if (nums.length == 0) return result;
+
+        int begin = nums[0];
+        int end = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (end + 1 == nums[i]) end++;
+            else {
+                sb = new StringBuilder();
+                if (begin == end) sb.append(begin);
+                else sb.append(begin).append("->").append(end);
+                result.add(sb.toString());
+                begin = nums[i];
+                end = nums[i];
+            }
+        }
+
+        sb = new StringBuilder();
+        if (begin == end) sb.append(begin);
+        else sb.append(begin).append("->").append(end);
+
+        result.add(sb.toString());
+        return result;
+    }
 }
