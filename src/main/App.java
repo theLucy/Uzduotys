13277fa3e89a33aa5d4ecbd44d9bbe40dfc3da88
Lucy <info@thelucy.lt>
2023-03-27 13:23:43 +0300

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,9 @@ package main;
 public class App {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+        System.out.println(sol.isPowerOfTwo(1)); // Expected true
+        System.out.println(sol.isPowerOfTwo(16)); // Expected true
+        System.out.println(sol.isPowerOfTwo(3)); // Expected false
+
     }
 }
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,9 @@
 package main;
 
 public class Solution {
-    
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        while (n % 2 == 0) n >>= 1;
+        return n == 1;
+    }
 }
