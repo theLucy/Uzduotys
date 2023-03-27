cec1c04bc62ef0339f7a293f17a186ee7251b30f
Lucy <info@thelucy.lt>
2023-03-27 15:45:05 +0300

    Solve task

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,10 @@ package main;
 public class App {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        
+        sol.isAnagram("anagram", "nagaram"); // Expected true
+        System.out.println("***");
+        sol.isAnagram("rat", "car"); // Expected false
+        System.out.println("***");
+        sol.isAnagram("aac", "cac"); // Expected false
     }
 }
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,14 @@
 package main;
 
+import java.util.Arrays;
+
 public class Solution {
-    
+    static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        char[] a1 = s1.toCharArray();
+        char[] a2 = s2.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        return Arrays.equals(a1, a2);
+    }
 }
