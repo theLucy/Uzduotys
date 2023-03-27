1871284e4add849030e0a2c45b69e3dd69f3a654
Lucy <info@thelucy.lt>
2023-03-27 14:54:19 +0300

    Solve task

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,13 @@
 package main;
 
 public class Solution {
-    
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val);
+            head = head.next;
+        }
+
+        return sb.toString().equals(sb.reverse().toString());
+    }
 }
