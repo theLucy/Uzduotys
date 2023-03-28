8e81901e6916fa1d7039708611b6b1c5648fb7ca
Lucy <info@thelucy.lt>
2023-03-28 14:49:13 +0300

    Solve task

--- src/main/java/dev/laurynas/Solution.java
+++ src/main/java/dev/laurynas/Solution.java
@@ -1,4 +1,19 @@
 package dev.laurynas;
 
 public class Solution {
+
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+
+        for (int factor : new int[] { 2, 3, 5 }) {
+            n = keepDividingWhenDivisible(n, factor);
+        }
+        return n == 1;
+    }
+    private int keepDividingWhenDivisible(int dividend, int divisor) {
+        while (dividend % divisor == 0) {
+            dividend /= divisor;
+        }
+        return dividend;
+    }
 }
--- src/test/java/dev/laurynas/SolutionTest.java
+++ src/test/java/dev/laurynas/SolutionTest.java
@@ -1,6 +1,31 @@
 package dev.laurynas;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
+
+    Solution sol = new Solution();
+
+    @Test
+    void sixShouldReturnTrue() {
+        assertTrue(sol.isUgly(6));
+    }
+
+    @Test
+    void oneShouldReturnTrue() {
+        assertTrue(sol.isUgly(1));
+    }
+
+    @Test
+    void fourteenShouldReturnFalse() {
+        assertFalse(sol.isUgly(14));
+    }
+
+    @Test
+    void number540124044ShouldReturnFalse() {
+        assertFalse(sol.isUgly(540124044));
+    }
+
 }
