c186e245e6c9ee3e7f78d997d1484ce300dc63ce
Lucy <info@thelucy.lt>
2023-03-28 13:16:45 +0300

    Solve task

--- src/main/java/dev/laurynas/Solution.java
+++ src/main/java/dev/laurynas/Solution.java
@@ -1,4 +1,30 @@
 package dev.laurynas;
 
 public class Solution {
+    public int addDigits(int num) {
+        return addDigitsRec(num);
+    }
+
+    public int addDigitsRec(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        sum = (sum < 10) ? sum : addDigits(sum);
+        return sum;
+    }
+
+    public int addDigitsIter(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+
 }
--- src/test/java/dev/laurynas/SolutionTest.java
+++ src/test/java/dev/laurynas/SolutionTest.java
@@ -1,6 +1,19 @@
 package dev.laurynas;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
+
+    Solution sol = new Solution();
+    @Test
+    void thirdyEightShouldReturnTwo() {
+        assertEquals(2, sol.addDigits(38));
+    }
+
+    @Test
+    void zeroShouldReturnZero() {
+        assertEquals(0, sol.addDigits(0));
+    }
 }
