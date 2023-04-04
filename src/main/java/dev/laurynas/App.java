82505e317860e02db69035f3565f6945f0931a8a
Lucy <info@thelucy.lt>
2023-04-04 16:16:53 +0300

    Solve task

--- src/main/java/dev/laurynas/App.java
+++ src/main/java/dev/laurynas/App.java
@@ -2,7 +2,5 @@ package dev.laurynas;
 
 public class App {
     public static void main(String[] args) {
-        Solution sol = new Solution();
-
     }
 }
--- src/test/java/dev/laurynas/SolutionTest.java
+++ src/test/java/dev/laurynas/SolutionTest.java
@@ -1,6 +1,32 @@
 package dev.laurynas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SolutionTest {
+
+    static Solution sol;
+
+    @BeforeAll
+    static void init() {
+        sol = new Solution();
+    }
+
+    @Test
+    void numberTwentySevenShouldReturnTrue() {
+        assertTrue(sol.isPowerOfThree(27));
+    }
+
+    @Test
+    void numberZeroShouldReturnFalse() {
+        assertFalse(sol.isPowerOfThree(0));
+    }
+
+    @Test
+    void numberMinus1ShouldReturnFalse() {
+        assertFalse(sol.isPowerOfThree(-1));
+    }
 }
