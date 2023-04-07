c5635ed76643beaff7853c24dba04ebb5d54bffa
Lucy <info@thelucy.lt>
2023-04-07 08:30:10 +0300

    Solve task

--- src/main/java/dev/laurynas/Solution.java
+++ src/main/java/dev/laurynas/Solution.java
@@ -1,4 +1,11 @@
 package dev.laurynas;
 
 public class Solution {
+    public boolean isPerfectSquare(int num) {
+        for (int i = 1; i < 46341; i++) {
+            if (i * i == num) return true;
+            if (i * i > num) return false;
+        }
+        return false;
+    }
 }
--- src/test/java/dev/laurynas/SolutionTest.java
+++ src/test/java/dev/laurynas/SolutionTest.java
@@ -1,6 +1,31 @@
 package dev.laurynas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SolutionTest {
+    static Solution sol;
+
+    @BeforeAll
+    static void init() {
+        sol = new Solution();
+    }
+
+    @Test
+    void sixteenShouldReturnTrue() {
+        assertTrue(sol.isPerfectSquare(16));
+    }
+
+    @Test
+    void fourteenShouldReturnFalse() {
+        assertFalse(sol.isPerfectSquare(14));
+    }
+
+    @Test
+    void bigNumberShouldReturnSmth() {
+        assertFalse(sol.isPerfectSquare(2147483647));
+    }
 }
