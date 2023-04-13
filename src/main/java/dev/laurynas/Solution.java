42d593e1773266e62528bd079029aa55f5f02081
Lucy <info@thelucy.lt>
2023-04-13 15:24:32 +0300

    Solve task

--- src/main/java/dev/laurynas/Solution.java
+++ src/main/java/dev/laurynas/Solution.java
@@ -1,4 +1,47 @@
 package dev.laurynas;
 
 public class Solution {
+
+    int pick;
+
+    public Solution(int pick) {
+        this.pick = pick;
+    }
+
+    public int guessNumber(int n) {
+        return guessNumberIter(n); // Iterative approach
+        //return guessNumberRec(n, 1, n); // Recursive approach
+    }
+
+    public int guessNumberIter(int n) {
+        int low = 1;
+        int high = n;
+        int mid = 0;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (guess(mid) == 1)
+                low = mid + 1;
+            else if (guess(mid) == -1)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    public int guessNumberRec(int n, int low, int high) {
+        int mid = low + (high - low) / 2;
+        if (guess(mid) == 1) {
+            return guessNumberRec(n, mid + 1, high);
+        } else if (guess(mid) == -1) {
+            return guessNumberRec(n, low, mid - 1);
+        } else {
+            return mid;
+        }
+    }
+
+    private int guess(int n) {
+        return Integer.compare(pick, n);
+    }
 }
--- src/test/java/dev/laurynas/SolutionTest.java
+++ src/test/java/dev/laurynas/SolutionTest.java
@@ -1,6 +1,25 @@
 package dev.laurynas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
+    @Test
+    void test1() {
+        Solution sol = new Solution(6);
+        assertEquals(6, sol.guessNumber(10));
+    }
+
+    @Test
+    void test2() {
+        Solution sol = new Solution(1);
+        assertEquals(1, sol.guessNumber(1));
+    }
+
+    @Test
+    void test3() {
+        Solution sol = new Solution(1);
+        assertEquals(1, sol.guessNumber(2));
+    }
 }
