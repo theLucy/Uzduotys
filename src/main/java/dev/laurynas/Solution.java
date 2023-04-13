1ce9644534557e4755a68dcc1ae81cc2ee321365
Lucy <info@thelucy.lt>
2023-04-13 15:58:36 +0300

    Solve task

--- src/main/java/dev/laurynas/Solution.java
+++ src/main/java/dev/laurynas/Solution.java
@@ -1,4 +1,15 @@
 package dev.laurynas;
 
 public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] usableLetters = new int[26];
+        for (char c : magazine.toCharArray())
+            usableLetters[c - 'a']++;
+
+        for (char c : ransomNote.toCharArray()) {
+            if (--usableLetters[c - 'a'] < 0)
+                return false;
+        }
+        return true;
+    }
 }
--- src/test/java/dev/laurynas/SolutionTest.java
+++ src/test/java/dev/laurynas/SolutionTest.java
@@ -1,6 +1,25 @@
 package dev.laurynas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SolutionTest {
+    static Solution sol = new Solution();
+
+    @Test
+    void test1() {
+        assertFalse(sol.canConstruct("a", "b"));
+    }
+
+    @Test
+    void test2() {
+        assertFalse(sol.canConstruct("aa", "ab"));
+    }
+
+    @Test
+    void test3() {
+        assertTrue(sol.canConstruct("aa", "aab"));
+    }
 }
